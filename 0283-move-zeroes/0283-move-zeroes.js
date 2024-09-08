6091815ffd452e75b7bec8bee55f903a17b9d68b
Sravan kumar polisetti <111481159+10srav@users.noreply.github.com>
2024-09-08 22:34:45 +0530

    Time: 71 ms (76.63%), Space: 53.1 MB (94.5%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,23 @@
+
+var moveZeroes = function(nums) {
+    let lastNonZeroIdx=0;
+    for( let i=0;i<nums.length;i++){
+        if(nums[i]!==0){
+            nums[lastNonZeroIdx]=nums[i];
+            lastNonZeroIdx++;
+        }
+    }
+    for(let i=lastNonZeroIdx;i<nums.length;i++){
+        nums[i]=0;
+    }
+
+
+
+
+
+            
+        
+    
+
+    
+};
